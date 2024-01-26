f1adfd0617965510e86495f136cf44f65ad216d4
Vivien DE BONA <vdebona@umanit.fr>
2024-01-26 19:38:24 +0100

    feat: add French schema
    - Rewrite the schema logic to use

---
+++ scripts/schemas/_index.js
@@ -0,0 +1,9 @@
+import { SCHEMA_EN } from './schema-en.js';
+import { SCHEMA_FR } from './schema-fr.js';
+import { SCHEMA_RU } from './schema-ru.js';
+
+export const SCHEMAS = {
+  en: SCHEMA_EN,
+  fr: SCHEMA_FR,
+  ru: SCHEMA_RU,
+};
